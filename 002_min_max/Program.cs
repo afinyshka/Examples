088acf325892f71c49e4e03240797206a578ac6d
afinyshka <afinyshka@mail.ru>
2022-04-25 11:13:06 +0300

    002 min max

---
+++ 002_min_max/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите два числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = a;
+int min = b;
+if (a>b)
+{
+    max=a;
+    min=b;
+}
+else
+{
+    max=b;
+    min=a;
+}
+Console.Write("max: ");
+Console.WriteLine(max);
+Console.Write("min: ");
+Console.WriteLine(min);
