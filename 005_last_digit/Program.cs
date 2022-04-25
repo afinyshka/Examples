0e2ee3ca602d769627316175713c51b68d7a1e8d
afinyshka <afinyshka@mail.ru>
2022-04-25 12:33:26 +0300

    005 Last digit rus c проверкойй

--- 005_last_digit/Program.cs
+++ 005_last_digit/Program.cs
@@ -1,5 +1,12 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 int a = int.Parse(Console.ReadLine());
-int digit = a % 10;
-Console.Write("Last digit:");
+if (a > 99 && a<1000)
+{
+    int digit = a % 10;
+Console.Write("Последний знак: ");
 Console.WriteLine(digit);
+}
+else
+{
+Console.WriteLine("Число не трехзначное ");
+}
