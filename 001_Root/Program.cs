6b1bf018a8c77da678057de6c4d7e87c777bfa5b
afinyshka <afinyshka@mail.ru>
2022-05-03 14:56:31 +0300

    new commit

--- 001_Root/Program.cs
+++ 001_Root/Program.cs
@@ -2,3 +2,8 @@
 int a = int.Parse(Console.ReadLine());
 Console.Write("Квадрат числа: ");
 Console.WriteLine(a * a);
+
+//Console.Write("Введите число: "); 
+//int a =  int.Parse(Console.ReadLine()); 
+//Console.Write("Квадрат числа: "); 
+//Console.WriteLine(a * a);
