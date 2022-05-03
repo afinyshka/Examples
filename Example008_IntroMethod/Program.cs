74bb75480cdfea7de48971dbb0e96606764ea757
afinyshka <afinyshka@mail.ru>
2022-05-03 13:37:05 +0300

    add function max from 3 from 3

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -6,7 +6,7 @@
     return result;
 }
 
-int a1 = 15;
+int a1 = 112115;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
@@ -16,10 +16,13 @@ int a3 = 13;
 int b3 = 23111;
 int c3 = 313;
 
-int max1 = Max (a1, b1, c1);
-int max2 = Max (a2, b2, c2);
-int max3 = Max (a3, b3, c3);
-int max = Max (max1, max2, max3);
+//int max1 = Max (a1, b1, c1);
+//int max2 = Max (a2, b2, c2);
+//int max3 = Max (a3, b3, c3);
+//int max = Max (max1, max2, max3);
 
-
+int max = Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3));
 Console.WriteLine(max);
