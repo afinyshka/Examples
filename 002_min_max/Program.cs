a7038bda8a6f12ba4edebdd5c9e5ac193baec996
afinyshka <afinyshka@mail.ru>
2022-05-09 23:15:37 +0300

    delite dodle prog

--- 002_min_max/Program.cs
+++ 002_min_max/Program.cs
@@ -17,22 +17,3 @@ Console.Write("max: ");
 Console.WriteLine(max); 
 Console.Write("min: "); 
 Console.WriteLine(min);
-Console.WriteLine("Введите два числа: ");
-int a = int.Parse(Console.ReadLine());
-int b = int.Parse(Console.ReadLine());
-int max = a;
-int min = b;
-if (a>b)
-{
-    max=a;
-    min=b;
-}
-else
-{
-    max=b;
-    min=a;
-}
-Console.Write("max: ");
-Console.WriteLine(max);
-Console.Write("min: ");
-Console.WriteLine(min);
