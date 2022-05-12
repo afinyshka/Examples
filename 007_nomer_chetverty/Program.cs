8b4d774133233d7eb177c9e4630d814fa43737d6
afinyshka <afinyshka@mail.ru>
2022-05-12 11:01:45 +0300

    add 007 koorditatnay ploskost

---
+++ 007_nomer_chetverty/Program.cs
@@ -0,0 +1,9 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+Console.WriteLine("Ведите координату x:");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите координату y:");
+int y = int.Parse(Console.ReadLine());
+if (x > 0 && y > 0 ) Console.WriteLine($"Число с координатами (х,y) принадлежит I четверти ");
+else if (x > 0 && y < 0 ) Console.WriteLine($"Число с координатами (х,y) принадлежит II четверти ");
+else if (x < 0 && y < 0 ) Console.WriteLine($"Число с координатами (х,y) принадлежит III четверти ");
+else if (x < 0 && y > 0 ) Console.WriteLine($"Число с координатами (х,y) принадлежит IV четверти ");
