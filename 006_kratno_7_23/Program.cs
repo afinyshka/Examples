1f8399f3ae67ea74f1a243c095502a06136c6508
afinyshka <afinyshka@mail.ru>
2022-05-12 10:47:27 +0300

    add fixed

--- 006_kratno_7_23/Program.cs
+++ 006_kratno_7_23/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Выяснить, кратно ли число 7 или 23:
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
-if (number%7 == 0) Console.WriteLine($"Число {number} кратно числу 7 ");
-else if (number%23 == 0) Console.WriteLine($"Число {number} кратно числу 23 ");
+if (number%7 == 0 || number%23 == 0) Console.WriteLine($"Число {number} кратно числу 7 или 23 ");
+//else if (number%23 == 0) Console.WriteLine($"Число {number} кратно числу 23 ");
 else 
 {
     Console.WriteLine($"Число {number} не кратно 7 или 23");
