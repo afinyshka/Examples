e33843f260454e4b32a727aae518cbf26673c105
afinyshka <afinyshka@mail.ru>
2022-05-16 11:05:25 +0300

    add 011 nimbers of digits

---
+++ 011_number_of_digits_in_NNNN/Program.cs
@@ -0,0 +1,11 @@
+﻿// Определить количество цифр в числе
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int tmp = number;
+int count = 0;
+while (number > 0) 
+{
+    number = number/10;
+    count++;
+}
+Console.WriteLine($"Number of digits of {tmp} = {count} ");
