b4eef96cc0a68726c2026c99de3a458dbd4cccdd
afinyshka <afinyshka@mail.ru>
2022-05-16 11:13:50 +0300

    add 012 product of numbers

---
+++ 012_Proizved_from_1_to_N/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int number1 = 1;
+int product = 1;
+Console.Write($"Product numbers from 1 to {number}: ");
+while (number1 <= number) 
+{
+    product = product * number1;
+    number1++;
+}
+Console.WriteLine($"{product} ");
