edd1d8bee43b699f695508181e018f57210983f4
afinyshka <afinyshka@mail.ru>
2022-05-16 10:23:38 +0300

    add 009 root on n number

---
+++ 009_root_or_number_1_N/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int number1 = 1;
+Console.Write($"Quares from 1 to N: ");
+while (number1 < number) 
+{
+    Console.Write($"{number1*number1} ");
+    number1++;
+}
