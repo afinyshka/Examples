8fdd1887850f9d585f0b24f53abdee41e66ab369
afinyshka <afinyshka@mail.ru>
2022-05-16 10:46:25 +0300

    add 010 sum from 1 to N

---
+++ 010_sum_from_1_to_N/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+int number = int.Parse(Console.ReadLine() ?? "0");
+int number1 = 1;
+int sum = 0;
+Console.Write($"Summa numbers from 1 to {number}: ");
+while (number1 <= number) 
+{
+    sum = sum + number1;
+    number1++;
+}
+Console.WriteLine($"{sum} ");
