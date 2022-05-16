5bc66df3e42a4c40619c60c579d4a91dbf949038
afinyshka <afinyshka@mail.ru>
2022-05-16 10:32:23 +0300

    add 009 root on n number2

--- 009_root_or_number_1_N/Program.cs
+++ 009_root_or_number_1_N/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Enter number: ");
 int number = int.Parse(Console.ReadLine() ?? "0");
 int number1 = 1;
-Console.Write($"Quares from 1 to N: ");
+Console.Write($"Quares from 1 to {number}: ");
 while (number1 <= number) 
 {
     Console.Write($"{number1*number1} ");
