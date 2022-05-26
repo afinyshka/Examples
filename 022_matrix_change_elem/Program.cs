527f04582b14bf86981268d3cb06d32bc216e39f
afinyshka <afinyshka@mail.ru>
2022-05-26 11:04:23 +0300

    add 022 + methods

--- 022_matrix_change_elem/Program.cs
+++ 022_matrix_change_elem/Program.cs
@@ -31,3 +31,36 @@ for (int row=0; row<n; row++)
     }
     Console.WriteLine();
 }
+
+//В двумерном массиве n×k заменить четные элементы на противоположные
+void FillArray(int[,] array)
+{
+    Random rnd           = new Random();
+    int    rowsLength    = array.GetLength(0);
+    int    columnsLength = array.GetLength(1);
+    for (int i = 0; i < rowsLength; i++)
+    {
+        for (int j = 0; j < columnsLength; j++)
+        {
+            array[i,j] = rnd.Next(-100,101);
+        }       
+    }
+}
+//--------------------------------------------------------------------------------------------+
+void ChangeEvenElements(int[,] array)
+{
+    int    rowsLength    = array.GetLength(0);
+    int    columnsLength = array.GetLength(1);
+    for (int i = 0; i < rowsLength; i++)
+    {
+        for (int j = 0; j < columnsLength; j++)
+        {
+            if(array[i,j]%2 == 0) array[i,j] = -array[i,j];
+        }       
+    }
+}
+//--------------------------------------------------------------------------------------------+
+void PrintArray(int[,] array)
+{
+    int    rowsLength    = array.GetLength(0);
+    int    columns
