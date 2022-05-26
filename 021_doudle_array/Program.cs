77dd153c99a317e388be235d3b53dd7e7f4569ff
afinyshka <afinyshka@mail.ru>
2022-05-26 10:42:47 +0300

    add 021 double array 2 idea

--- 021_doudle_array/Program.cs
+++ 021_doudle_array/Program.cs
@@ -30,3 +30,38 @@ for (int row=0; row<n; row++)
 //     }
 //     Console.WriteLine();
 // }
+
+//from Alexandra:
+
+// Показать двумерный массив размером m×n заполненный целыми числами
+void PrintArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();    
+    }
+} 
+
+void FillArray(int[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); 
+        }  
+    }
+} 
+Console.WriteLine("Введите число m, n: ");
+int m = int.Parse(Console.ReadLine()??"0");
+int n = int.Parse(Console.ReadLine()??"0");
+
+int[,] matrix = new int [m,n];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
