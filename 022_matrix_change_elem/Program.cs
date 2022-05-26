065ca55bf945f43cfb16b995d32ec7b695b34635
afinyshka <afinyshka@mail.ru>
2022-05-26 10:57:53 +0300

    add 022 matrix even change

---
+++ 022_matrix_change_elem/Program.cs
@@ -0,0 +1,33 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+
+Console.WriteLine("Enter your number n");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter your number m");
+int m = int.Parse(Console.ReadLine() ?? "0");
+int [,] matrix21 = new int[n,m];
+for (int row=0; row<n; row++)
+{
+    for (int column=0; column<m; column++)
+    {
+        matrix21[row, column] = new Random().Next(1,10);
+    }
+}
+
+for (int row=0; row<n; row++)
+{
+    for (int column=0; column<m; column++)
+    {
+        Console.Write(matrix21[row, column] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int row=0; row<n; row++)
+{
+    for (int column=0; column<m; column++)
+    {
+        if (matrix21[row, column]%2==0) matrix21[row, column] = matrix21[row, column] * -1;
+        Console.Write(matrix21[row, column] + " ");
+    }
+    Console.WriteLine();
+}
