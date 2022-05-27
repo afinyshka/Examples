8c35e72f049adb44b1b6b4ee795f16c4dbe5c22b
afinyshka <afinyshka@mail.ru>
2022-05-27 16:08:50 +0300

    add 020 find fibbonachi less given number1

--- 020_Fibbonachi_less_N/Program.cs
+++ 020_Fibbonachi_less_N/Program.cs
@@ -16,3 +16,30 @@ while (fibbonachi1 + fibbonachi2 < number)
     Console.Write($"{resultFib} ");
 }
 Console.WriteLine();
+
+//Показать первые N чисел Фибоначи  
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int a = 1; int b = 0; int result;
+for (int i = 0; i < n; i++)
+{
+    result = a + b;
+    Console.WriteLine(result + " ");
+    a = b;
+    b = result;
+}
+
+// Показать числа Фибоначчи меньше заданного числа N 
+Console.Write("Введите число: "); 
+int N = int.Parse(Console.ReadLine() ?? "");
+int firstValue = 0;
+int secondValue = 1;
+int result1 = 0;
+Console.Write($"{firstValue} {secondValue} ");
+while (result1 > N)
+{
+    result1 = firstValue + secondValue;
+    if (result1 > N) break; Console.Write(result1 + " ");
+    firstValue = secondValue;
+    secondValue = result1;
+}
