bc10416e7709a71e576f4774959ab4333375b312
afinyshka <afinyshka@mail.ru>
2022-06-06 10:39:45 +0300

    add 029 recursion number drom 1 to N

---
+++ 029_numbers_frim1_to_N/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+int NumbersFromOneToNumber (int n)
+{
+    Console.WriteLine(n);
+    if (n==1)return 1;
+    else return NumbersFromOneToNumber(n-1);
+}
+
+NumbersFromOneToNumber(8);
