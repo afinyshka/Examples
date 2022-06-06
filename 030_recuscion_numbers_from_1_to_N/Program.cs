e9c21c57bf6781cafcd060afd5c0c1ce0e41257c
afinyshka <afinyshka@mail.ru>
2022-06-06 11:01:44 +0300

    add 030 recursion from 1 to n

---
+++ 030_recuscion_numbers_from_1_to_N/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+int NumbersFromOneToNumber (int start, int finish)
+{
+    Console.WriteLine($"number = {start}");
+    if (start==finish)return finish;
+    else return NumbersFromOneToNumber(start+1, finish);
+}
+
+NumbersFromOneToNumber(1, 6);
