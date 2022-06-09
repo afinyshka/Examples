75f2382bcbbd1e01b4be172e633c8f98bf285d04
afinyshka <afinyshka@mail.ru>
2022-06-09 11:06:45 +0300

    add 031 recursion sums of degits

---
+++ 031_sums_of_digits/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int SumOfDigits(int number)
+{
+    Console.WriteLine(number);
+    if (number < 1) return number;
+    else return number%10 + SumOfDigits(number / 10);
+    // 749356%10 + 74935%10 + 7493%10 + 749%10 + 74%10 + 7%10 + SumOfDigits(7 / 10);
+    // 749356%10 + 74935%10 + 7493%10 + 749%10 + 74%10 + 7%10 + 0;
+}
+Console.WriteLine($"Summa = {SumOfDigits(749356)}" );
