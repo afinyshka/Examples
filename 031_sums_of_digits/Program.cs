d81563eab18b9ee35ce31046e824a21d43656c62
afinyshka <afinyshka@mail.ru>
2022-06-09 11:31:07 +0300

    add 031 recursion sums of degits new

--- 031_sums_of_digits/Program.cs
+++ 031_sums_of_digits/Program.cs
@@ -2,9 +2,10 @@
 
 int SumOfDigits(int number)
 {
-    Console.WriteLine(number);
-    if (number < 1) return number;
+    Console.WriteLine($"{number} {number%10} ");
+    if (number < 1 ) return number;
     else return number%10 + SumOfDigits(number / 10);
+    // return n < 10 ? number : number % 10 + dSumOfDigits( number / 10 );
     // 749356%10 + 74935%10 + 7493%10 + 749%10 + 74%10 + 7%10 + SumOfDigits(7 / 10);
     // 749356%10 + 74935%10 + 7493%10 + 749%10 + 74%10 + 7%10 + 0;
 }
