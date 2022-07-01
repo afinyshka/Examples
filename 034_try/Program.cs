d8357555e1c5c8c7c8e4614a57a8305b8e1a1623
afinyshka <afinyshka@mail.ru>
2022-07-01 15:20:44 +0300

    i try hw with diagram

---
+++ 034_try/Program.cs
@@ -0,0 +1,26 @@
+﻿// напечатать ноый массив где его элементы текст  и менее или равно 3 символам
+
+string [] texset = {"hollo", "54", "s34l", "git", "876", };
+Console.WriteLine($"number of elements = {texset.Length}");
+int count=0;
+for (int i=0; i<texset.Length; i++)
+{
+    if (texset[i].Length<=3)
+    {
+        Console.WriteLine(texset[i]);
+        count++;
+    }
+}
+Console.WriteLine($"count = {count} ");
+
+string [] newTexset = new string [count];
+for (int i=0; i<texset.Length; i++)
+{
+    int j=0;
+    if (texset[i].Length<=3)
+    {
+        newTexset[j] = texset[i];
+        Console.Write($"{newTexset[j]} ");
+        j++;
+    }
+}
